7ad1752e559af0d5e11c98b6bece26234a4d3b23
Lucas Louca <lucaslouca@users.noreply.github.com>
2015-12-15 08:26:43 +0100

    Create Singleton.java

---
+++ problems/Singleton.java
@@ -0,0 +1,13 @@
+package problems;
+
+public class Singleton {
+	private Singleton() {}
+	
+	private static class SingletonHelper {
+		private static final Singleton INSTANCE = new Singleton();
+	}
+	
+	public static Singleton getInstance() {
+		return SingletonHelper.INSTANCE;
+	}
+}
