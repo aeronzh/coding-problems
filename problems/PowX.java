5bcb5ed9780eb4eb5ebbe87dd8b09f3bbc14beef
Lucas Louca <lucaslouca@users.noreply.github.com>
2015-12-18 08:22:38 +0100

    Create PowX.java

---
+++ problems/PowX.java
@@ -0,0 +1,36 @@
+package com.lucaslouca.other;
+
+/**
+ * Implement pow(x, n).
+ * 
+ * @author lucas
+ *
+ */
+public class PowX {
+
+	public static double pow(double x, int n) {
+		if (n == 0) {
+			return 1;
+		}
+
+		// divide into sub problems of half the size
+		// e.g.: 6^5 = 6^2 * 6^2 * 6
+		double t = pow(x, n / 2);
+		if (n % 2 == 0) {
+			return t * t;
+		} else {
+			if (n > 0) {
+				return t * t * x;
+			} else {
+				//5^-3 = 1 ÷ 5 ÷ 5 ÷ 5
+				//5^-3 could also be calculated like: 1 ÷ (5 × 5 × 5) 
+				return (t * t) / x;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(pow(5, 2));
+	}
+
+}
