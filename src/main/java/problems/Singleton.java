d7d4eb09c291d405fda460ba4169c8f990a9733c
Lucas Louca <lucaslouca@users.noreply.github.com>
2016-02-08 10:54:17 +0100

    Update Singleton.java

--- src/main/java/problems/Singleton.java
+++ src/main/java/problems/Singleton.java
@@ -1,14 +1,31 @@
 package problems;
 
-// Bill Pugh's solution
+/**
+ * Bill Pugh's solution
+ * 
+ * @author lucas
+ *
+ */
 public class Singleton {
-	private Singleton() {}
-	
-	private static class SingletonHelper {
+	private Singleton() {
+	}
+
+	private static class SingletonHolder {
 		private static final Singleton INSTANCE = new Singleton();
 	}
-	
+
+	/**
+	 * The first time getInstance() is called, the JVM will hold the holder
+	 * class. If another thread calls getInstance() concurrently, the JVM won't
+	 * load the holder class a second time: it will wait for the first thread to
+	 * have completed the class loading, and at the end of the loading and
+	 * initialization of the holder class, both threads will see the holder
+	 * class properly initialized and thus containing the unique singleton
+	 * instance.
+	 * 
+	 * @return
+	 */
 	public static Singleton getInstance() {
-		return SingletonHelper.INSTANCE;
+		return SingletonHolder.INSTANCE;
 	}
 }
