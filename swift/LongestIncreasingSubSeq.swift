b98d24a200c04d9493477ee5734f87571937135f
Lucas Louca <lucaslouca@users.noreply.github.com>
2016-03-04 14:09:39 +0100

    Update LongestIncreasingSubSeq.swift

--- swift/LongestIncreasingSubSeq.swift
+++ swift/LongestIncreasingSubSeq.swift
@@ -1,5 +1,4 @@
 var sequences:[(tail:Int, size:Int)] = []
-var smallest:Int!
 
 func solve(array:[Int]) -> Int {
     
@@ -8,15 +7,39 @@ func solve(array:[Int]) -> Int {
             sequences.append((tail:elem, size:1))
         } else {
             // Case 1: elem is smallest so far. Start a new sequence
-            let smallest = sequences.filter(<#includeElement: (T) -> Bool##(T) -> Bool#>)
+            let smallest = sequences.minElement({$0.tail > $1.tail})
             
-            // Case 2: elem is larger than all the tails so far. Find largest sequence so far and extend it.
-            
-            // Case 3: elem is in neither the smallest, nor the largest.  Find the list whose tail is largest but still smaller than elem. Extend that list. Delete all other lists that have the same size.
+            if (elem < smallest?.tail) {
+                sequences.append((tail:elem, size:1))
+            } else {
+                // Case 2: elem is larger than all the tails so far. 
+                // Find longest sequence so far and extend it.
+                let largestTail = sequences.maxElement({$0.tail < $1.tail})
+                if (elem > largestTail?.tail) {
+                    let longest = sequences.maxElement({$0.size < $1.size})
+                    let extendedSeq = (tail:elem, size:longest!.size+1)
+                    if let idx = sequences.indexOf({$0.tail == longest!.tail && $0.size == longest!.size}) {
+                        sequences[idx] = extendedSeq
+                    }
+                } else {
+                    // Case 3: elem is in neither the smallest, nor the largest.  
+                    // Find the list whose tail is largest but still smaller than elem. 
+                    // Extend that list. Delete all other lists that have the same size.
+                    let minMax = (sequences.filter() {$0.tail < elem}).maxElement({$0.tail < $1.tail})
+                    let extendedSeq = (tail:elem, size:minMax!.size+1)
+                    if let idx = sequences.indexOf({$0.tail == minMax!.tail && $0.size == minMax!.size}) {
+                        sequences = sequences.filter() {$0.size == extendedSeq.size} 
+                        sequences[idx] = extendedSeq
+                    }
+                    
+                }
+            }
         }
     }
     
-    return 1
+    
+    return  sequences.maxElement({$0.size < $1.size})!.size
 }
 
 print(solve([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15])) // 6
+// 0, 2, 6, 9, 11, 15
