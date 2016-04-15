7c0e55877d92ff56a2a322bf58c35dc3e58e2435
Lucas Louca <lucaslouca@users.noreply.github.com>
2016-04-15 08:57:30 +0200

    Update SamAndSubstrings.java

--- src/main/java/problems/SamAndSubstrings.java
+++ src/main/java/problems/SamAndSubstrings.java
@@ -2,6 +2,7 @@ package problems;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
@@ -54,74 +55,53 @@ public class SamAndSubstrings {
 		System.out.println();
 	}
 
-	public static long pow(long x, long n, long p) {
-		long result = 1;
-		while (n > 0) {
-			if ((n & 1) == 1) {
-				result = (result * x) % p;
-			}
-
-			x = (x * x) % p;
-			n = n >> 1;
-		}
-
-		return result;
-	}
-
-	public static long c(int n, int k, long p) {
-		// C(n,k) = n!/(k!*(n-k)!)
-
-		long numerator = 1;
-		for (int i = 0; i < k; i++) {
-			numerator = (numerator * (n - i)) % p;
-		}
-
-		long denominator = 1;
-		for (int i = 1; i <= k; i++) {
-			denominator = (denominator * i) % p;
-		}
-
-		return (numerator * (pow(denominator, p - 2, p))) % p;
-	}
-	
-	private static long substrings(String str) {
+	private static long brute(String str) {
 		long sum = 0;
-		
+
 		char[] ch = str.toCharArray();
-		for (int i=0; i<=ch.length; i++) {
-			for (int j=i; j<=ch.length; j++) {
-				String tmp = str.substring(i,j);
+		for (int i = 0; i <= ch.length; i++) {
+			for (int j = i; j <= ch.length; j++) {
+				String tmp = str.substring(i, j);
 				if (!tmp.isEmpty()) {
 					long num = Long.parseLong(tmp);
 					sum = (sum + num) % MODULO;
 				}
 			}
 		}
-		
+
 		return sum;
 	}
 
-	/*
-	 * 123
-	 * 
-	 * 1 2 3 12 23 123 = 164
-	 * 1 3 6 18 41 164
-	 * 
-	 * 
-	 * Example :
-	 * 
-	 * current char is '3' and previous sum is 3. Then the current sum is 3+3=6.
-	 * 
-	 * current char is '2' (from substr 12) and the previous sum is 6 and len of substr is 2.
-	 * 
-	 * 
-	 * dp[][] with dp[i][j] = sum if ith character is equal to j. Example dp[2][2] = 
-	 */
-	
-	private static void solve(int[] a) {
-		
+	//
+	// Example:
+	// 123
+	// 
+	// Substrings:
+	// 1 2 3 12 23 123
+	// 
+	// Digit   Unit                          Ten                   Hundred
+	// '1'      1                             1                      1  
+	// '2'      2 ('2','12')                  2 ('23', '123')
+	// '3'      3 ('3', '23', '123')
+	// 
+	//  '1': 1*1*111
+	//  '2': 2*2*11
+	//  '3': 3*3*1
+	//
+	private static BigInteger solve(int[] a) {
+		BigInteger sum = BigInteger.ZERO;
+
+		BigInteger x = BigInteger.ONE;
+		for (int i = a.length - 1; i >= 0; i--) {
+			BigInteger digit = new BigInteger("" + a[i]);
+			BigInteger index = new BigInteger("" + (i + 1));
+			sum = sum.add(digit.multiply(index).multiply(x)).mod(new BigInteger("" + MODULO));
+			x = x.multiply(new BigInteger("10")).add(BigInteger.ONE).mod(new BigInteger("" + MODULO));
+		}
+
+		return sum;
 	}
-	
+
 	public static void main(String[] args) throws FileNotFoundException {
 		System.setIn(new FileInputStream(System.getProperty("user.home") + "/" + "in.txt"));
 		Scanner outputScanner = new Scanner(new FileInputStream(System.getProperty("user.home") + "/" + "out.txt"));
@@ -132,9 +112,9 @@ public class SamAndSubstrings {
 		char[] splitted = line.toCharArray();
 		int[] a = new int[splitted.length];
 		for (int i = 0; i < splitted.length; i++) {
-			a[i] = splitted[i] - 48;
+			a[i] = splitted[i] - '0';
 		}
-		
-		System.out.println(substrings("123"));
+
+		System.out.println(solve(a));
 	}
 }
