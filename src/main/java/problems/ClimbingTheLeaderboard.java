1a766555ce34bc9669d2b702dc0d75b6d1a8b03f
Luke <lucaslouca@users.noreply.github.com>
2016-12-18 21:54:00 +0100

    Create ClimbingTheLeaderboard.java

---
+++ src/main/java/problems/ClimbingTheLeaderboard.java
@@ -0,0 +1,48 @@
+package problems;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ClimbingTheLeaderboard {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream(System.getProperty("user.home") + "/" + "in.txt"));
+        Scanner in = new Scanner(System.in);
+ 
+        int n = in.nextInt();
+        int[] score = new int[n];
+        for (int i = 0; i < n; i++) {
+            score[i] = in.nextInt();
+        }
+ 
+        int m = in.nextInt();
+        int[] alice = new int[m];
+        for (int i = 0; i < m; i++) {
+            alice[i] = in.nextInt();
+        }
+ 
+        int maxRank = 1;
+        for (int i = 1; i < n; i++) {
+            if (score[i] != score[i - 1]) {
+                maxRank++;
+            }
+        }
+ 
+        int rank = maxRank + 1;
+        int cursor = n - 1;
+        for (int a = 0; a < m; a++) {
+            while (cursor >= 0 && alice[a] >= score[cursor]) {
+                boolean flag = false;
+                do {
+                    cursor--;
+                    if (!flag) {
+                        rank = Math.max(1, rank - 1);
+                        flag = true;
+                    }
+                } while (cursor > 0 && score[cursor + 1] == score[cursor]);
+            }
+ 
+            System.out.println(rank);
+        }
+    }
+}
