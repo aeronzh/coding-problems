b36a82cdbfe8b5a228b7ba770e1984357d66b202
Luke <lucaslouca@users.noreply.github.com>
2017-04-27 15:03:13 +0200

    Update ThePowerSum.java

--- src/main/java/problems/ThePowerSum.java
+++ src/main/java/problems/ThePowerSum.java
@@ -2,21 +2,17 @@ package problems;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class ThePowerSum {
-	private static int solve(int x, int n, Set<Integer> seq, int num, double sum) {
+	private static int solve(int x, int n, int num, double sum) {
 		if (sum == x) {
 			return 1;
 		} else {
 			int ans = 0;
 			if (sum < x) {
 				for (int i = num; i <= Math.pow(x, 1.0 / n); i++) {
-					seq.add(i);
-					ans += solve(x, n, seq, i + 1, sum + Math.pow(i, n));
-					seq.remove(i);
+					ans += solve(x, n, i + 1, sum + Math.pow(i, n));
 				}
 			}
 			return ans;
@@ -30,6 +26,6 @@ public class ThePowerSum {
 		int x = in.nextInt();
 		int n = in.nextInt();
 
-		System.out.println(solve(x, n, new HashSet<Integer>(), 1, 0));
+		System.out.println(solve(x, n, 1, 0));
 	}
 }
